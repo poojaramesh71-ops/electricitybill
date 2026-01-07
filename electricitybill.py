12fc4727e9aee15fc38cc30b1269eeedd9ab1828
Pooja Ramesh <kumbarp71@gmail.com>
2026-01-07 18:21:45 +1345

    Add files via upload

---
+++ electricitybill.py
@@ -0,0 +1,15 @@
+def calculate_electricity_bill(units):
+    if units <= 100:
+        bill = units * 2
+    elif units <= 200:
+        bill = (100 * 2) + ((units - 100) * 4)
+    else:
+        bill = (100 * 2) + (100 * 4) + ((units - 200) * 6)
+
+    return bill
+
+
+units = int(input("Enter number of units consumed: "))
+
+total_bill = calculate_electricity_bill(units)
+print("Total Electricity Bill: ₹", total_bill)
